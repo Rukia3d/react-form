76373bf8a049c922eb1b6ffd386172c34e37a40d
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-08-07 20:41:36 +1000

    Initial test setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+test('renders without crashing', () => {
+  const { getByText } = render(
+    <App />
+  );
 });
