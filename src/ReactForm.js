bb7aee152a3b8928edd52bafa9150dc9f032e2d4
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-08-07 20:45:47 +1000

    ReactForm and ReactForm test

---
+++ src/ReactForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class ReactForm extends React.Component{
+    constructor(props){
+      super(props);
+    }
+
+    render(){
+      return(
+        <h1>ReactForm</h1>
+      )
+    }
+
+}
+
+export default ReactForm;
--- src/ReactForm.test.js
+++ src/ReactForm.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ReactForm from './ReactForm';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 
 afterEach(cleanup);
 
 test('renders without crashing', () => {
   const { getByText } = render(
-    <App />
+    <ReactForm />
   );
 });
