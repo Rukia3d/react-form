128b38ee0201caece9e438b79e94ef745c0e6476
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-08-07 21:06:31 +1000

    Initial test returns correct input

--- src/ReactForm.test.js
+++ src/ReactForm.test.js
@@ -31,10 +31,14 @@ let output = {
 
 test('submitting the form', () => {
   const onSubmit = jest.fn();
-  const { getByText } = render(
+  const { getByText, getByLabelText } = render(
     <ReactForm input={ input } output={onSubmit}/>
   );
 
+  const testInput = getByLabelText("Data");
+  testInput.value = "data";
+  
   fireEvent.click(getByText("Submit"));
   expect(onSubmit.mock.calls.length).toBe(1);
+  expect(onSubmit.mock.calls[0]).toEqual(["data"]);
 });
