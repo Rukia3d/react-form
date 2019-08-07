7f06fb2d9f5dbedd12332c0d6fe856e689791f27
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-08-07 20:51:26 +1000

    ReactForm test1 red

--- src/ReactForm.test.js
+++ src/ReactForm.test.js
@@ -5,8 +5,35 @@ import { render, fireEvent, cleanup } from '@testing-library/react';
 
 afterEach(cleanup);
 
-test('renders without crashing', () => {
+let input = [
+  { name: "textForm" },
+  { dob: "textForm" },
+  { gender: "textForm" },
+]
+
+let output = {
+    name: "John Foo",
+    dob: "1990-01-01",
+    gender: 1,
+    contact: [{
+        type: "mobile",
+        value: "0400123123"
+    },{
+        type: "home",
+        value: "610000000"
+    }],
+    guardian: {
+        name: "Jane Foo",
+        contact: "0400123123"
+    }
+}
+
+test('submitting the form', () => {
+  const onSubmit = jest.fn();
   const { getByText } = render(
-    <ReactForm />
+    <ReactForm input={ input } onSubmit={onSubmit}/>
   );
+
+  fireEvent.click(getByText("Submit"));
+  expect(onSubmit.mock.calls.length).toBe(1);
 });
