e672b13751cd05a84f9d0ffe340e7c6121af7bc1
Inga Pflaumer <inga.pflaumer@gmail.com>
2019-08-11 14:58:12 +1000

    More information on Higher-Order Component withJSONInput

--- src/ReactForm/withJSONInput.js
+++ src/ReactForm/withJSONInput.js
@@ -4,6 +4,14 @@ import PropTypes from 'prop-types';
 /*
 Higher-Order Component that parses input prop as a JSON string and passes the
 result down to the original component.
+
+Motivation for HOC: the test task doesn't have enough information on how the
+component will be used and where JSON input string comes from, but as its
+parsing/handling is not directly related to the task of rendering the form
+itself - I extracted it into this HOC.
+
+It separates these two concerns, and it will allow to implement things like
+dealing with JSON errors here easily, without complicating form rendering code.
 */
 
 function withJSONInput(Component) {
